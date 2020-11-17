bcaca108130199b389a876faf2b38ef0d6338d01
Kazuki Hayase <k.hayase@buysell-technologies.com>
2020-11-17 18:22:49 +0900

    フラッシュメッセージの変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     if @task.save
       redirect_to tasks_path
-      flash[:success] = "タスクの作成が完了しました！"
+      flash[:success] = "タスクを作成しました！"
     else
       render "new"
     end
@@ -29,7 +29,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     if @task.update_attributes(task_params)
       redirect_to @task
-      flash[:success] = "タスクの編集が完了しました！"
+      flash[:success] = "タスクを編集しました！"
     else
       render "edit"
     end
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
   
   def destroy
     Task.find(params[:id]).destroy
-    flash[:success] = "タスクの削除が完了しました！"
+    flash[:success] = "タスクを削除しました！"
     redirect_to tasks_path
   end
   
