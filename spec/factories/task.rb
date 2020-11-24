2f63774de03bb0ffe3476f98500809a624330de3
KazukiHayase <k.hayase@buysell-technologies.com>
2020-11-24 17:56:46 +0900

    タスクの表示順のテストの修正

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,16 +1,6 @@
 FactoryBot.define do
-  factory :task1, class: Task do
-    name { "タスク1" }
-    content  { "タスク1です" }
-  end
-
-  factory :task2, class: Task do
-    name { "タスク2" }
-    content  { "タスク2です" }
-  end
-
-  factory :task3, class: Task do
-    name { "タスク3" }
-    content  { "タスク3です" }
+  factory :task do
+    sequence(:name) { |n| "task#{n}" }
+    content  { "これはタスクの説明です" }
   end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,18 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
-    before do
-        @task1 = create(:task1)
-        @task2 = create(:task2)
-        @task3 = create(:task3)
-    end
+    let!(:task1) { create(:task) }
+    let!(:task2) { create(:task) }
+    let!(:task3) { create(:task) }
 
     it "タスクが作成日時の降順で並んでいること" do
         visit tasks_path
         task = all(".task")
 
-        expect(task[0].find("h3").text).to eq @task3.name
-        expect(task[1].find("h3").text).to eq @task2.name
-        expect(task[2].find("h3").text).to eq @task1.name
+        expect(task[0].find("h3").text).to eq task3.name
+        expect(task[1].find("h3").text).to eq task2.name
+        expect(task[2].find("h3").text).to eq task1.name
     end
 end
