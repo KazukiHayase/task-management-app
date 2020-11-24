b6c297177965569bb05ff4e43ac6b9af4f2aec9f
KazukiHayase <k.hayase@buysell-technologies.com>
2020-11-24 14:46:26 +0900

    タスクの表示順序のテストのリファクタリング

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
     before do
-        @task1 = Task.create!(name: "タスク1", content: "タスク1です")
-        @task2 = Task.create!(name: "タスク2", content: "タスク2です")
-        @task3 = Task.create!(name: "タスク3", content: "タスク3です")
+        @task1 = create(:task1)
+        @task2 = create(:task2)
+        @task3 = create(:task3)
     end
 
     it "タスクが作成日時の降順で並んでいること" do
         visit tasks_path
         task = all(".task")
 
-        expect(task[0].find("h3").text).to eq "タスク3"
-        expect(task[1].find("h3").text).to eq "タスク2"
-        expect(task[2].find("h3").text).to eq "タスク1"
+        expect(task[0].find("h3").text).to eq @task3.name
+        expect(task[1].find("h3").text).to eq @task2.name
+        expect(task[2].find("h3").text).to eq @task1.name
     end
 end
