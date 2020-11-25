6eb127b3a8593f9e95660781a9a64e7d06b3a41e
KazukiHayase <k.hayase@buysell-technologies.com>
2020-11-25 13:20:48 +0900

    タスクのモデルテスト追加

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  let(:task) {create(:task)}
+
+  subject { task } 
+
+  it "タスクは有効" do
+    is_expected.to be_valid
+  end
+
+  context "タスクが無効な時" do
+    it "nameが空なら無効" do
+      task.name = nil
+      is_expected.to be_invalid
+    end
+
+    it "contentが空なら無効" do
+      task.content = nil
+      is_expected.to be_invalid
+    end
+
+    it "nameが51文字以上なら無効" do
+      task.name = "a" * 51
+      is_expected.to be_invalid
+    end
+    
+    it "contentが201文字以上なら無効" do
+      task.content = "a" * 201
+      is_expected.to be_invalid
+    end
+  end
+  
+  
+end
