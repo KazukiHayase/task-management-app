342b80d36f9e2052906218d149b1a46f07f86ec0
KazukiHayase <k.hayase@buysell-technologies.com>
2020-11-25 16:48:51 +0900

    deadlineのバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ApplicationRecord
     validates :name, presence: true, length: {maximum: 50}
     validates :content, presence: :true, length: {maximum:200}
+    validate  :date_not_before_today
+
+    private
+    
+    def date_not_before_today
+        if deadline.present? && deadline < Date.today
+            errors.add(:deadline, "は今日以降を選択してください")
+        end
+    end
 end
