f8614d623518abc289ed3152008a1b8a23d51d10
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-01 18:48:02 +0900

    taskテーブルのnameとstatusにインデックス追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_053429) do
+ActiveRecord::Schema.define(version: 2020_12_01_093119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_12_01_053429) do
     t.datetime "updated_at", null: false
     t.date "deadline"
     t.integer "status"
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
