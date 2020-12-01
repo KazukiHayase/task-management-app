6c5723cdadc987b6060effd68ef26b767649da9b
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-01 18:13:14 +0900

    モデルの検索機能のテスト追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,37 +2,69 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   let(:task) {create(:task)}
+  let(:task_doing) {create(:task, :doing)}
 
-  subject { task } 
+  describe "有効性" do
+    subject { task } 
 
-  it "タスクは有効" do
-    is_expected.to be_valid
+    it "タスクは有効" do
+      is_expected.to be_valid
+    end
+
+    context "タスクが無効な時" do
+      it "nameが空なら無効" do
+        task.name = nil
+        is_expected.to be_invalid
+      end
+
+      it "contentが空なら無効" do
+        task.content = nil
+        is_expected.to be_invalid
+      end
+
+      it "nameが51文字以上なら無効" do
+        task.name = "a" * 51
+        is_expected.to be_invalid
+      end
+      
+      it "contentが201文字以上なら無効" do
+        task.content = "a" * 201
+        is_expected.to be_invalid
+      end
+
+      it "締め切り日が今日よりも前のなら無効" do
+        task.deadline -= 10
+        is_expected.to be_invalid
+      end
+    end
   end
+  
 
-  context "タスクが無効な時" do
-    it "nameが空なら無効" do
-      task.name = nil
-      is_expected.to be_invalid
+  describe "検索機能" do
+    let(:search_params) { {keyword: "タスク", status: 1} } 
+
+    before do
+      task
+      task_doing
     end
 
-    it "contentが空なら無効" do
-      task.content = nil
-      is_expected.to be_invalid
+    subject { Task.search(search_params) } 
+
+    it "ステータスが１のタスクが検索される" do
+      is_expected.to include(task)
     end
 
-    it "nameが51文字以上なら無効" do
-      task.name = "a" * 51
-      is_expected.to be_invalid
-    end
-    
-    it "contentが201文字以上なら無効" do
-      task.content = "a" * 201
-      is_expected.to be_invalid
+    it "ステータスが2のタスクは検索されない" do
+      is_expected.to_not include(task_doing)
     end
 
-    it "締め切り日が今日よりも前の時" do
-      task.deadline -= 10
-      is_expected.to be_invalid
+    it "「タスク」が名前に入ってると検索される" do
+      is_expected.to include(task)
+    end
+
+    it "「タスク」が名前に入ってないと検索されない" do
+      task.update_attributes(name: "こんにちは")
+      is_expected.to_not include(task)
     end
   end
 end
