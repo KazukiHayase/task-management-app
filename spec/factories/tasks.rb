a1f0fb7271a8aa662c179fa6509e92ef00b3d32f
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-01 17:46:06 +0900

    検索機能のテスト追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,5 +3,14 @@ FactoryBot.define do
     sequence(:name) { |n| "タスク#{n}" }
     content  { "これはタスクの説明です" }
     sequence(:deadline, Date.today)
+    status {1}
+
+    trait :doing do
+      status{2}
+    end
+
+    trait :done do
+      status{3}
+    end
   end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", js: true, type: :system do
     let(:tasks) { create_list(:task, 3) } 
+    let(:task_doing) { create(:task, :doing) } 
+    let(:task_done) { create(:task, :done) } 
     
 
-    describe "タスク一覧画面でソート" do
+    describe "ソート機能" do
         before do
             tasks
             visit tasks_path
@@ -24,6 +26,31 @@ RSpec.describe "Tasks", js: true, type: :system do
         it "タスクが締め切り日の降順で並んでいること" do
             sort("締め切り日が遠い順")
             is_expected.to have_content tasks[2].name
+        end
+    end
+
+    describe "検索機能" do
+        before do
+            tasks
+            task_doing
+            task_done
+            visit tasks_path
+        end
+
+        it "ステータスで検索" do
+            select("着手中", from: "status")
+            expect(page).to  have_content task_doing.name
+        end
+
+        it "タイトルで検索" do
+            fill_in("キーワード", with: "タスク")
+            expect(page).to  have_content tasks[0].name
+        end
+
+        it "ステータスとタイトルで検索" do
+            select("完了", from: "status")
+            fill_in("キーワード", with: "タスク")
+            expect(page).to  have_content task_done.name
         end
     end
 end
