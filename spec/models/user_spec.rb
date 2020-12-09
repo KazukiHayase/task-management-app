cdcde52d62a82a525c2417b545914cb2d8be895c
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-09 15:52:07 +0900

    UserのModelspecのリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,87 +17,96 @@ RSpec.describe User, type: :model do
     context "全ての要素が有効な時" do
       it_behaves_like "ユーザーは有効"
     end
+
+    describe "name" do
+      before do
+        user.name = name
+      end
+      
+      context "nameが空の時" do
+        let(:name) { nil } 
+        it_behaves_like "ユーザーは無効"
+      end
+
+      context "nameが50文字以下の時" do
+        let(:name) { "a" * 50 } 
+        it_behaves_like "ユーザーは有効"
+      end
+
+      context "nameが51文字以上の時" do
+        let(:name) { "a" * 51 } 
+        it_behaves_like "ユーザーは無効"
+      end
+    end
     
-    context "nameが空の時" do
-      before {user.name = nil}
-      it_behaves_like "ユーザーは無効"
-    end
+    describe "email" do
+      before do
+        user.email = email
+      end
+      
+      context "emailが空の時" do
+        let(:email) { nil } 
+        it_behaves_like "ユーザーは無効"
+      end
 
-    context "emailが空の時" do
-      before {user.email = nil}
-      it_behaves_like "ユーザーは無効"
-    end
+      context "emailが50文字以下の時" do
+        let(:email) { "a" * 41 + "@test.com" }
+        it_behaves_like "ユーザーは有効"
+      end
 
-    context "passwordが空の時" do
-      let(:user) {build(:user, password: nil, password_confirmation: nil)}
-      it_behaves_like "ユーザーは無効"
+      context "emailが51文字以上の時" do
+        let(:email) { "a" * 51 + "@test.com" }
+        it_behaves_like "ユーザーは無効"
+      end
+
+      context "emailが無効な値の時" do
+        let(:email) { "test.com" }
+        it_behaves_like "ユーザーは無効"
+      end
+
+      context "emailが重複してる時" do
+        let(:other_user) { create(:user) } 
+        let(:email) { other_user.email }
+        it_behaves_like "ユーザーは無効"
+      end
     end
     
-    context "nameが50文字以下の時" do
-      before {user.name = "a" * 50}
-      it_behaves_like "ユーザーは有効"
-    end
-
-    context "nameが51文字以上の時" do
-      before {user.name = "a" * 51}
-      it_behaves_like "ユーザーは無効"
-    end
-    context "emailが50文字以下の時" do
-      before {user.email = "a" * 41 + "@test.com"}
-      it_behaves_like "ユーザーは有効"
-    end
-
-    context "emailが51文字以上の時" do
-      before {user.email = "a" * 51 + "@test.com"}
-      it_behaves_like "ユーザーは無効"
-    end
-
-    context "emailが無効な値の時" do
-      before {user.email = "test.com"}
-      it_behaves_like "ユーザーは無効"
-    end
-
-    context "emailが重複してる時" do
-      let(:other_user) { create(:user) } 
-      before {user.email = other_user.email}
-      it_behaves_like "ユーザーは無効"
-    end
-
-    context "passwordが5文字以下の時" do
+    describe "password" do
       before do
-        user.password = "a" * 5
-        user.password_confirmation = "a" * 5
+        user.password = password
+        user.password_confirmation = password
       end
-      it_behaves_like "ユーザーは無効"
-    end
-
-    context "passwordが6文字以上72文字以下の時" do
-      before do
-        user.password = "a" * 6
-        user.password_confirmation = "a" * 6
+      
+      context "passwordが空の時" do
+        let(:password) { nil } 
+        it_behaves_like "ユーザーは無効"
       end
-      it_behaves_like "ユーザーは有効"
-    end
 
-    context "passwordが6文字以上72文字以下の時" do
-      before do
-        user.password = "a" * 72
-        user.password_confirmation = "a" * 72
+      context "passwordが5文字以下の時" do
+        let(:password) {"a" * 5}
+        it_behaves_like "ユーザーは無効"
       end
-      it_behaves_like "ユーザーは有効"
-    end
 
-    context "passwordが73文字以上の時" do
-      before do
-        user.password = "a" * 73
-        user.password_confirmation = "a" * 73
+      context "passwordが6文字以上72文字以下の時" do
+        let(:password) {"a" * 6}
+        it_behaves_like "ユーザーは有効"
       end
-      it_behaves_like "ユーザーは無効"
-    end
 
-    context "passwordとpassword_confirmationが一致しない時" do
-      before {user.password_confirmation = "wordpass"}
-      it_behaves_like "ユーザーは無効"
+      context "passwordが6文字以上72文字以下の時" do
+        let(:password) {"a" * 72}
+        it_behaves_like "ユーザーは有効"
+      end
+
+      context "passwordが73文字以上の時" do
+        let(:password) {"a" * 73}
+        it_behaves_like "ユーザーは無効"
+      end
+
+      context "passwordとpassword_confirmationが一致しない時" do
+        let(:password) { "password" } 
+        before {user.password_confirmation = "wordpass"}
+        it_behaves_like "ユーザーは無効"
+      end
     end
   end
 end
