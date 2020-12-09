a5c21e8384e4ff0f019a4e853a9aa1bb5247f048
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-09 16:08:59 +0900

    ログイン機能のroute作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :tasks 
   post "tasks/sort", to: "tasks#sort", as: "sort_tasks"
   post "tasks/search", to: "tasks#search", as: "search_tasks"
+  get  "login", to: "sessions#new"
+  post  "login", to: "sessions#create"
+  delete  "logout", to: "sessions#destroy"
 end
