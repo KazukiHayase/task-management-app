ba0f0fa8c0528023429ae8421bb2303b1271d808
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-09 12:05:32 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: {maximum: 50}
+    VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :email, presence: true, length: {maximum: 50}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+    has_secure_password validations: true
+    validates :password, length: {minimum: 6}
 end
