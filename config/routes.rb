9ee91a6d975ed4c416f770a0e2cb4c92044dd12c
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-15 14:35:59 +0900

    無駄なルート削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :tasks 
   post "tasks/sort", to: "tasks#sort", as: "sort_tasks"
   post "tasks/search", to: "tasks#search", as: "search_tasks"
