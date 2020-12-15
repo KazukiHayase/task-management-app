7fde3c53cefb1055e750e008f1ba7b86434bba4e
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-15 16:09:18 +0900

    機能変更に伴うテストの修正

--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -3,3 +3,10 @@ def sort(text)
     page.evaluate_script("$('#sorttasks_select').trigger('change')")
     sleep 1
 end
+
+def login(user)
+    visit login_path
+    fill_in "メールアドレス", with: user.email 
+    fill_in "パスワード", with: user.password 
+    click_button "ログイン"
+end
