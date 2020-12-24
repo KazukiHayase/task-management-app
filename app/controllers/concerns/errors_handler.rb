337cf925db6df6b8728e4f82f83785b0784232e5
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-24 16:52:55 +0900

    ステータスコード500のエラーページ作成

--- app/controllers/concerns/errors_handler.rb
+++ app/controllers/concerns/errors_handler.rb
@@ -6,6 +6,7 @@ module ErrorsHandler
     end
 
     included do
+        rescue_from StandardError, with: :render_500
         rescue_from ActionController::RoutingError, with: :render_404
         rescue_from ActiveRecord::RecordNotFound, with: :rescue_404
     end
@@ -19,6 +20,19 @@ module ErrorsHandler
                 render json: { error: '404 error' }, status: :not_found
             else
                 render 'errors/404', status: :not_found
+            end
+        end
+
+        def render_500(e = nil)
+            if e
+                logger.error "Rendering 500 with exception: #{e.message}"
+                @message = e.message
+            end
+            
+            if request.format.to_sym == :json
+                render json: { error: '500 error' }, status: :internal_server_error
+            else
+                render 'errors/500', status: :internal_server_error
             end
         end
 end
