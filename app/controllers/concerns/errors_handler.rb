f3504dcce96978f4473677178a23d36b60f74ffe
KazukiHayase <k.hayase@buysell-technologies.com>
2020-12-24 17:40:49 +0900

    step24で実装した例外のハンドリング追加

--- app/controllers/concerns/errors_handler.rb
+++ app/controllers/concerns/errors_handler.rb
@@ -7,8 +7,9 @@ module ErrorsHandler
 
     included do
         rescue_from StandardError, with: :render_500
+        rescue_from ApplicationError::NotPermittedError, with: :redirect_tasks_or_login
         rescue_from ActionController::RoutingError, with: :render_404
-        rescue_from ActiveRecord::RecordNotFound, with: :rescue_404
+        rescue_from ActiveRecord::RecordNotFound, with: :render_404
     end
 
     private
@@ -34,5 +35,12 @@ module ErrorsHandler
             else
                 render 'errors/500', status: :internal_server_error
             end
+        end
+
+        def redirect_tasks_or_login(e = nil)
+            logger.info "Redirecting with exception: #{e.message}" if e
+
+            flash[:danger] = "閲覧権限がありません"
+            redirect_to tasks_path
         end
 end
