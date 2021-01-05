d533b8eb8769fe2544defd79c52df8f27a68aa42
KazukiHayase <k.hayase@buysell-technologies.com>
2021-01-05 09:59:24 +0900

    errors_handlerのインデント修正

--- app/controllers/concerns/errors_handler.rb
+++ app/controllers/concerns/errors_handler.rb
@@ -14,30 +14,30 @@ module ErrorsHandler
 
     private
 
-        def render_404(e = nil)
-            logger.info "Rendering 404 with exception: #{e.message}" if e
+    def render_404(e = nil)
+        logger.info "Rendering 404 with exception: #{e.message}" if e
 
-            if request.format.to_sym == :json
-                render json: { error: '404 error' }, status: :not_found
-            else
-                render 'errors/404', status: :not_found
-            end
+        if request.format.to_sym == :json
+            render json: { error: '404 error' }, status: :not_found
+        else
+            render 'errors/404', status: :not_found
         end
+    end
 
-        def render_500(e = nil)
-            logger.error "Rendering 500 with exception: #{e.message}" if e
-            
-            if request.format.to_sym == :json
-                render json: { error: '500 error' }, status: :internal_server_error
-            else
-                render 'errors/500', status: :internal_server_error
-            end
+    def render_500(e = nil)
+        logger.error "Rendering 500 with exception: #{e.message}" if e
+        
+        if request.format.to_sym == :json
+            render json: { error: '500 error' }, status: :internal_server_error
+        else
+            render 'errors/500', status: :internal_server_error
         end
+    end
 
-        def redirect_tasks_or_login(e = nil)
-            logger.info "Redirecting with exception: #{e.message}" if e
+    def redirect_tasks_or_login(e = nil)
+        logger.info "Redirecting with exception: #{e.message}" if e
 
-            flash[:danger] = "閲覧権限がありません"
-            redirect_to tasks_path
-        end
+        flash[:danger] = "閲覧権限がありません"
+        redirect_to tasks_path
+    end
 end
